5255074bee82bf05e0abe84f0a37a6a17bf6ee5f
Jonas Tranberg <jonastranberg93@gmail.com>
2021-06-03 13:32:57 +0200

    maybe

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-files=$(git show --pretty="" --name-only -- '**.php')
+files=$(git diff-tree --no-commit-id --name-status --diff-filter=d -r ${{ github.event.pull_request.base.sha }}..${{ github.event.after }})
+echi $files
 if [ ! -z "$files" ]; then
     phpcs --standard=PSR12 $files
 fi
